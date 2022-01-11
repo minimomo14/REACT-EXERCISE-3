691bedff138cd46dcb968e8506fd9ea7f4f433b7
Momo Miller <minimomo14z@hotmail.com>
2022-01-11 12:30:27 -0500

    curent updates

--- app-name/src/component/UpDownCounter.tsx
+++ app-name/src/component/UpDownCounter.tsx
@@ -1,37 +1,15 @@
 import React, { useState } from "react";
 import "./UpDownCounter.css"
 
-
-function count(num) {
-    var s = "";
-    for(var i = 1; i <= num; i++) {
-      s += i;
-  
-      if (i < (num)) {
-        s += ', ';
-      }
-    }
-    return s;
-  }
-
-function UpDownCounter(num) {
-    const [upDownCounter, setUpDownCounter] = useState(false);
-    let clickButton = "";
-    for (let i = 0, i<num; i++) {
-        clickButton =+ 1;
-    } if (upDownCounter === true) {
-        //up by 1
-    }
-    // else (Down by 1);
-    // create buttons that...
-    // set Up by +1
-    // set Down by -1
-    return(
+function UpDownCounter() {
+    const [upDownCounter, setUpDownCounter] = useState(0);
+        return(
         <div className="UpDownCounter">
-            {upDownCounter ?
-            <button onClick={() => setUpDownCounter(true)}>Up</button>:
-            <button onClick={() => setUpDownCounter(false)}>Down</button>
-            }
+
+            <button onClick={() => setUpDownCounter((count)=> count+1)}>Up</button>
+            <h2>Count: {upDownCounter}</h2>
+            <button onClick={() => setUpDownCounter((count)=> count-1)}>Down</button>
+            
         </div>
     );
 };
